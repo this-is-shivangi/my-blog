2f9482b889ff99ec7e07dd39abec84c7c24ef377
shivangi <shivangishukla114@gmail.com>
2020-09-08 23:49:28 +0530

    my first blog

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+# Create your models here.
+class Post(models.Model):
+
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now)
+    publish_date =  models.DateTimeField(blank=True,null=True)
+
+
+    def publish(self):
+        self.publish_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
---
+++ myblog/skey.py
@@ -0,0 +1,3 @@
+def key():
+    k= "0l7u7^6)!a(gxnww(b5a4ie!c)+v)17z+sswss3qcifu=!ltbj"
+    return k
