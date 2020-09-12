e47aa8b66070f3471479bbdbedf83bdcba36ad25
shivangi shukla <shivangishukla114@gmail.com>
2020-09-12 22:53:52 +0530

    details page

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,6 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
     return render(request, 'home.html',{"posts":posts})
+
+def post_details(request):
+    return render(request, 'detail.html',{})
